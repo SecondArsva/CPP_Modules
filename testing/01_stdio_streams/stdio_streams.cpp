5a902466586e4949f68b449af9ed7d699c6ec91a
OMEN <david-g-v-1997@hotmail.com>
2024-12-23 04:23:35 +0100

    01_stdio_streams

---
+++ testing/01_stdio_streams/stdio_streams.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+	char	buff[512];
+
+	std::cout << "¡Hola mundo!" << std::endl;
+
+	std::cout << "Introduce una palabra" << std::endl;
+	std::cin >> buff;
+	std::cout << "Has introducido: ["<< buff <<"]" << std::endl;
+
+	return (0);
+}
