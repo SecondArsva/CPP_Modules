dd6167d853e90b2edbef3063a94bad7c1c8479e3
OMEN <david-g-v-1997@hotmail.com>
2025-01-23 08:09:28 +0100

    Module 02: ex00: finished

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,48 @@
+#include "Fixed.hpp"
+
+// Default Constructor
+Fixed::Fixed():_fixedPointValue(0)
+{
+	std::cout << "Default constuctor called" << std::endl;
+}
+
+// Constructor Overload
+Fixed::Fixed(int const n):_fixedPointValue(n)
+{
+	std::cout << "Overload constructor called with the value " << n << std::endl;
+}
+
+// Copy Constructor
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy destructor called" << std::endl;
+	*this = src;
+}
+
+// Copy assignment operator overload
+Fixed &Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		this->_fixedPointValue = rhs.getRawBits();
+	return (*this);
+}
+
+// Destructor
+Fixed::~Fixed()
+{
+	std::cout << "Destuctor called" << std::endl;
+}
+
+// Getter
+int Fixed::getRawBits(void)const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
+}
+
+// Setter
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixedPointValue = raw;
+}
