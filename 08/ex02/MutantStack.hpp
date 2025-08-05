6cb5cd3a0947d30f73487686a811ec0c2a4e1423
OMEN <david-g-v-1997@hotmail.com>
2025-08-05 23:40:09 +0200

    Module_08: ex02: finished?

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -1,8 +1,8 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-#include <iostream>
-#include <stack>
+# include <iostream>
+# include <stack>
 
 template<typename T>
 class MutantStack : public std::stack<T>
